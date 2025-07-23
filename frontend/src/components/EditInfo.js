37324c618f9ea91f435e4936ce5b7a5da1a54507
Cassandra Sziklai <cns58@cornell.edu>
2025-07-22 23:27:03 -0400

    make ui look better

--- frontend/src/components/EditInfo.js
+++ frontend/src/components/EditInfo.js
@@ -1,8 +1,10 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import { useState, useEffect } from 'react';
+import Paper from '@mui/material/Paper';
 
 const EditInfo = () => {
+    const paperStyle = {padding: '50px 20px', width:600, margin: "20px auto"}
     const location = useLocation();
     const navigate = useNavigate();
     const [name, setName] = useState(location.state.name);
@@ -26,22 +28,26 @@ const EditInfo = () => {
 
     return (
         <div className="edit-info">
+            <Paper elevation={3} style = {paperStyle}>
             <h1>Edit Student Information</h1>
-            <form onSubmit={handleUpdate}>
+            <form onSubmit={handleUpdate} style={{ marginTop: '20px' }}>
                 <input
                     type="text"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     placeholder="Full Name"
+                    style={{ marginRight: '10px' }}
                 />
                 <input
                     type="email"
                     value={address}
                     onChange={(e) => setAddress(e.target.value)}
                     placeholder="Email"
+                    style={{ marginRight: '10px' }}
                 />
                 <Button type="submit">Update</Button>
             </form>
+            </Paper>
         </div>
     );
 }
